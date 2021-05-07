7728e978a73546f57ce9b8d31c32bfacf0457e56
lijunfeng <junfeng.li@hand-china.com>
2021-05-07 14:22:56 +0800

    fix: 修改宽高

--- src/screenshot/screenshot.js
+++ src/screenshot/screenshot.js
@@ -3,11 +3,17 @@ const path = require('path');
 
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    defaultViewport:{
+      width:1680,
+      height:960
+    }
+  });
+
   const page = await browser.newPage();
   await page.goto('http://coderralh-blocks.open.hand-china.com/templates/common-draggable-demo?name=common-draggable&from=%252Ftemplates%252Fcommon-draggable-demo');
-  // setTimeout(async() => {
-  // }, 2000);
-  await page.screenshot({ path: path.join(__dirname, 'example.png'), fullPage: true });
-  await browser.close();
+  setTimeout(async() => {
+    await page.screenshot({ path: path.join(__dirname, 'example1.png'), fullPage: true });
+    await browser.close();
+  }, 2000);
 })();
